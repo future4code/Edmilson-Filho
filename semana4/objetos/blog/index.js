ff8fa7a35d4bc6f8ca7c62e8abcf6bebd50e8935
EdmilsonFilho <edmilsonfof@gmail.com>
2020-10-08 11:32:38 -0300

    Desafios melhorados

--- semana4/objetos/blog/index.js
+++ semana4/objetos/blog/index.js
@@ -2,14 +2,12 @@ let tituloPost = document.getElementById("titulo-post");
 let autorPost = document.getElementById("autor-post");
 let conteudoPost = document.getElementById("conteudo-post");
 let imagemPost = document.getElementById("imagem-post");
-// let listaPosts = document.getElementById("lista-de-posts");
 let botaoCriar = document.getElementById("botao-criar");
 let formulario = document.getElementById("formulario");
 
 const posts = [];
 
 criarPost = () => {
-    // e.preventDefault();
     
     if (tituloPost.value !== "" && autorPost.value !== "" && conteudoPost.value !== "") {
         const post = {
@@ -21,25 +19,7 @@ criarPost = () => {
 
         posts.push(post);
         console.log(posts);
-        // salvarPost(post);
-        // window.location.assign("posts.html");
     } else {
         alert("O(s) campo(s) está(ão) vazio(s). Por favor, preencha-o(s).");
     }
 }
-
-// salvarPost = (post) => {
-//     containerPosts.innerHTML += `
-//     <div>
-//     <h2>${post.titulo}</h2>
-//     <div>${post.autor}</div>
-//     <p>${post.conteudo}</p>
-//     <img src="${post.imagem}">
-//     </div>
-//     `;
-// }
-
-// botaoCriar.onclick = criarPost;
-
-// containerPosts.innerHTML += `<li><h2>${tituloPost.value}</h2><div>${autorPost.value}</div><p>${conteudoPost.value}</p></li>`;
-// tituloPost.value, tituloPost.value, tituloPost.value, tituloPost.value = "";
--- semana4/objetos/blog/posts.js
+++ semana4/objetos/blog/posts.js
@@ -1,33 +1,49 @@
-// salvarPost = (post) => {
-//     containerPosts.innerHTML += `
-//     <div>
-//     <h2>${post.titulo}</h2>
-//     <div>${post.autor}</div>
-//     <p>${post.conteudo}</p>
-//     <img src="${post.imagem}">
-//     </div>
-//     `;
-// }
 let containerPosts = document.getElementById("container-de-posts");
 
 let params = (new URL(document.location)).searchParams;
 
 const post = {
+    id: localStorage.length,
     titulo: params.get("titulo"),
     autor: params.get("autor"),
     conteudo: params.get("conteudo"),
     imagem: params.get("imagem")
 }
 
-        salvarPost = (post) => {
-            containerPosts.innerHTML += `
-            <div>
-            <h2>${post.titulo}</h2>
-            <div>${post.autor}</div>
-            <p>${post.conteudo}</p>
-            <img src="${post.imagem}">
-            </div>
-            `;
+const posts = [];
+
+posts.push(post);
+
+salvarPost = (id) => {
+    if (localStorage.length > 0) {
+        localStorage.setItem(`post${id + 1}`, JSON.stringify(post));
+    } else {
+        localStorage.setItem(`post1`, JSON.stringify(post));
+    }
 }
 
-salvarPost(post);
+console.log(JSON.stringify(post));
+
+salvarPost(localStorage.length);
+
+let postsRecebidos = [];
+
+if (localStorage.length >= 0) {
+    for (let i = 1; i < localStorage.length + 1; i++) {
+        postsRecebidos.push(JSON.parse(localStorage.getItem(`post${i}`)));
+        console.log(JSON.parse(localStorage.getItem(`post${i}`)));
+    }
+}
+
+for (let item of postsRecebidos) {
+    containerPosts.innerHTML += `
+        <div>
+        <h2>${item.titulo}</h2>
+        <div>${item.autor}</div>
+        <p>${item.conteudo}</p>
+        <img src="${item.imagem}">
+        </div>
+    `;
+}
+
+console.log(postsRecebidos);
