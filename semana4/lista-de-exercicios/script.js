39da6227d37985e49d454d6b285c3434ba87e786
EdmilsonFilho <edmilsonfof@gmail.com>
2020-10-09 16:59:14 -0300

    Exercicio 1 concluido

---
+++ semana4/lista-de-exercicios/script.js
@@ -0,0 +1,3 @@
+// Exercício 1
+
+// O código recebe o valor 100 como parâmetro, pede ao usuario para informar a cotacao do dolar, retorna o valor recebido como parametro multiplcado por 100 e exibido em reais e no final imprimi o resultado
