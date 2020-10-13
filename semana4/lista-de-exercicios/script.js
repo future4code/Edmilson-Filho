95fd8d84f02ca4c57a7c49e75cf5390e2c5db1a1
EdmilsonFilho <edmilsonfof@gmail.com>
2020-10-13 12:13:27 -0300

    Exercicio 2 concluido

--- semana4/lista-de-exercicios/script.js
+++ semana4/lista-de-exercicios/script.js
@@ -1,3 +1,7 @@
 // Exercício 1
 
 // O código recebe o valor 100 como parâmetro, pede ao usuario para informar a cotacao do dolar, retorna o valor recebido como parametro multiplcado por 100 e exibido em reais e no final imprimi o resultado
+
+// Exercicio 2
+
+// Recebe uma string com um tipo de investimento e um valor como parâmetros para determinar o valor após o investimento de acordo com o tipo de investimento
