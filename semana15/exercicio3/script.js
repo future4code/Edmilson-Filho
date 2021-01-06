d610ab7ebc33ca8948df30415d85f5828b0858b9
EdmilsonFilho <edmilsonfof@gmail.com>
2021-01-05 23:56:06 -0300

    Exercicio melhorado

--- semana15/exercicio3/script.js
+++ semana15/exercicio3/script.js
@@ -1,15 +1,26 @@
+var fs = require('fs');
+
 // Exercício 3
 
-const tarefaNova = process.argv[2];
-const tarefas = [];
+const tarefaNova = process.argv[3];
 
 const exibirTarefas = (tarefaNova) => {
-    tarefas.push(tarefaNova);
 
-    fs.appendFile('tarefas.txt', tarefas, 'utf8');
-    tarefasFile = fs.readFile('tarefas.txt', 'utf8');
+    fs.open('tarefas.txt', tarefaNova, (err, fd) => {
+        if (err) throw err;
+        fs.appendFile(fd, 'data to append', 'utf8', (err) => {
+          fs.close(fd, (err) => {
+            if (err) throw err;
+          });
+          if (err) throw err;
+        });
+      });
 
-    console.log("tarefas: ", tarefasFile);
+    // fs.readFile('/tarefas.txt', 'utf8',  (err, data) => {
+    //     if (err) throw err;
+    //     console.log(data);
+    // }
+    // );
 }
 
 exibirTarefas(tarefaNova);
