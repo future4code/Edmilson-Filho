e06f005578acde47e79bbc0478e9b0e1b1adb6e8
EdmilsonFilho <edmilsonfof@gmail.com>
2021-01-07 16:23:07 -0300

    Exercicio concluido

--- semana15/expressjs/src/index.ts
+++ semana15/expressjs/src/index.ts
@@ -1,19 +1,63 @@
-import express, {Express} from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { AddressInfo } from 'net';
 import { countries } from './countries';
 
-const app: Express = express();
+const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get("countries/all", (req: Request, res: Response) => {
-    try {
-        const result: country[] = countries;
-    } catch {
+type country = {
+    id: number,
+    name: string,
+    capital: string,
+    continent: string
+ }
 
+app.get('/countries/all', (req: Request, res: Response ) => {
+    const result: any | undefined = countries.map(
+        country => ({
+            id: country.id,
+            name: country.name
+        })
+    )
+    
+    res.status(200).send(result)
+})
+
+app.get("/countries/:id", (req: Request, res: Response) => {
+    const result: country | undefined = countries.find(
+        country => country.id === Number(req.params.id)
+    );
+
+    if (result) {
+        res.status(200).send(result)
+     } else {
+        res.status(404).send("Not found")
+     }
+
+    res.status(200).send(result);
+})
+
+app.get('/countries/search', (req: Request, res: Response) => {
+    let result: country[] = countries;
+    
+    try {
+        if (!req.query.length) {
+            throw new Error("Nenhum parâmetro foi passado");
+        }
+
+        result = result.filter(
+            country => country.name.includes(req.query.name as string) &&
+            country.capital.includes(req.query.capital as string) &&
+            country.continent.includes(req.query.continent as string)
+        )
+    } catch {
+        throw new Error("Nenhum parâmetro foi passado");
     }
+
+    res.status(200).send(result)
 })
 
 const server = app.listen(process.env.PORT || 3003, () => {
