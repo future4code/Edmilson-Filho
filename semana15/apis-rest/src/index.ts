2956e56fd638b71f03c564229c3e509b21d3b941
EdmilsonFilho <edmilsonfof@gmail.com>
2021-01-07 20:42:13 -0300

    Exercicio concluido

--- semana15/apis-rest/src/index.ts
+++ semana15/apis-rest/src/index.ts
@@ -1,79 +1,194 @@
-//importando express com Request e Response e cors
-import express, {Request, Response} from 'express';
-import cors from 'cors';
+// Exercicio 1
 
-//extra: importando configuração de rede do node
-import { AddressInfo } from "net";
-//iniciando a aplicação web com express
+// A) Metódo GET
+// B) "/users"
+
+// Exercicio 2
+
+// A) Depois do path eu coloquei search?type=<o type>
+// B) Sim
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import { AddressInfo } from 'net';
+import { users } from './users';
+
 const app = express();
 
-//ativando os módulos de Bodyparser e cors
 app.use(express.json());
 app.use(cors());
 
-type user = {
+enum UserType {
+    NORMAL = "NORMAL",
+    ADMIN = "ADMIN"
+}
+
+type users = {
     id: number,
     name: string,
     email: string,
-    type: string,
+    type: UserType,
     age: number
 }
 
-
-let users: user[] = [
-    {
-        id: 1,
-        name: "Alice",
-        email: "alice@email.com",
-        type: "ADMIN",
-        age: 12
-    },
-    {
-        id: 2,
-        name: "Bob",
-        email: "bob@email.com",
-        type: "NORMAL",
-        age: 36
-    },
-    {
-        id: 3,
-        name: "Coragem",
-        email: "coragem@email.com",
-        type: "NORMAL",
-        age: 21
-    },
-    {
-        id: 4,
-        name: "Dory",
-        email: "dory@email.com",
-        type: "NORMAL",
-        age: 17
-    },
-    {
-        id: 5,
-        name: "Elsa",
-        email: "elsa@email.com",
-        type: "ADMIN",
-        age: 17
-    },
-    {
-        id: 6,
-        name: "Fred",
-        email: "fred@email.com",
-        type: "ADMIN",
-        age: 60
+app.get("/users/search", (req: Request, res: Response) => {
+    let errorCode: number = 400;
+    
+    try {
+        if (!req.query.type) {
+            errorCode = 400;
+            throw new Error();
+        }
+        
+        const result = users.filter(user => user.type === req.query.type );
+        
+        if (!result.length) {
+            errorCode = 404;
+            throw new Error();
+        }
+        
+        res.status(200).send(result);
+    } catch {
+        res.status(errorCode).send("Tipo de usuário inexistente.");
     }
-]
+})
 
 
+// Exercicio 3
+// A) path param
+app.get("/users/:name", (req: Request, res: Response) => {
+    let errorCode: number = 400;
+    
+    try {
+        if (!req.params.name) {
+            errorCode = 400;
+            throw new Error();
+        }
+        
+        const result = users.filter(user => user.name === req.params.name);
+        
+        if (!result.length) {
+            errorCode = 404;
+            throw new Error();
+        }
+        
+        res.status(200).send(result);
+    } catch {
+        // B)
+        res.status(errorCode).send("Usuário não encontrado.");
+    }
+})
 
+
+// Exercicio 4
+// A) Nada
+// B) Sim, porque até onde eu sei, não há diferença. Além disso, torna o app mais consistente
+app.put("/users", (req: Request, res: Response) => {
+    let errorCode: number = 400;
+    
+    try {
+        if (!req.body.name || !req.body.email || !req.body.type || !req.body.age) {
+            errorCode = 400;
+            throw new Error();
+        }
+        
+        const newUser: users = {
+            id: Date.now(),
+            name: req.body.name,
+            email: req.body.email,
+            type: req.body.type,
+            age: req.body.age,
+        }
+
+        users.push(newUser);
+
+        res.status(200).send(newUser);
+    } catch {
+        res.status(errorCode).send("Usuário não encontrado.");
+    }
+})
+
+// Exercicio 5
+app.put("/users/:id", (req: Request, res: Response) => {
+    let errorCode: number = 400;
+    
+    try {
+        if (!req.body.name) {
+            errorCode = 400;
+            throw new Error();
+        }
+
+        const result = users.find(user => user.id === Number(req.params.id));
+
+        if (result) {
+            result.name = `${req.body.name} -ALTERADO`
+        } else {
+            errorCode = 404;
+        }
+
+        res.status(200).send(result);
+    } catch {
+        res.status(errorCode).send("Usuário não encontrado.");
+    }
+})
+
+// Exercicio 6
+app.patch("/users/:id", (req: Request, res: Response) => {
+    let errorCode: number = 400;
+    
+    try {
+        if (!req.params.id) {
+            errorCode = 400;
+            throw new Error();
+        }
+
+        if (!req.body.name) {
+            errorCode = 400;
+            throw new Error();
+        }
+
+        const result = users.find(user => user.id === Number(req.params.id));
+
+        if (result) {
+            result.name = `${req.body.name} -REALTERADO`
+        } else {
+            errorCode = 404;
+        }
+
+        res.status(200).send(result);
+    } catch {
+        res.status(errorCode).send("Usuário não encontrado.");
+    }
+})
+
+// Exercicio 7
+app.delete("/users/:id", (req: Request, res: Response) => {
+    let errorCode: number = 400;
+    
+    try {
+        if (!req.params.id) {
+            errorCode = 400;
+            throw new Error();
+        }
+
+        const result = users.find(user => user.id === Number(req.params.id));
+
+        if (result) {
+            users.splice(result.id, 1)
+        } else {
+            errorCode = 404;
+        }
+
+        res.status(200).send(result);
+    } catch {
+        res.status(errorCode).send("Usuário não encontrado.");
+    }
+})
 
 const server = app.listen(process.env.PORT || 3003, () => {
     if (server) {
-       const address = server.address() as AddressInfo;
-       console.log(`Server is running in http://localhost: ${address.port}`);
+        const address = server.address() as AddressInfo;
+        console.log(`Server is running in http://localhost: ${address.port}`);
     } else {
-       console.error(`Failure upon starting server.`);
+        console.error("Failure upon starting server.");
     }
-  });
-  
+})
